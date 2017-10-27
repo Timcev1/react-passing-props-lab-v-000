035db43c46d1185f5595ee6f55eaab054b1e191b
Timothy Cevallos <timcev@gmail.com>
2017-10-27 20:31:20 +0000

    automatically backed up by learn

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 
 const Filter = ({filters, handleChange}) => {
-    return (
-      <select onChange={this.props.handleChange} defaultValue='all'>
-        <option value='all'>All</option>
-        {this.state.filters.map(filter =>
-          <option key={filter} value={filter}>{filter}</option>
-        )}
-      </select>
-    );
-  }
+  <select onChange={handleChange} defaultValue='all'>
+    <option value='all'>All</option>
+    {filters.map(filter =>
+      <option key={filter} value={filter}>{filter}</option>
+    )}
+  </select>;
+
 Filter.defaultProps = {
   filters: [],
-  handleChange: null
-}
+  handleChange: function() {}
+};
+
 export default Filter;
