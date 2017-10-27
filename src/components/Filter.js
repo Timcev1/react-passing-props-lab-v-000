74157b9758b098071b8f7632a4d08a59b90341ac
Timothy Cevallos <timcev@gmail.com>
2017-10-27 20:20:06 +0000

    automatically backed up by learn

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,25 +1,6 @@
 import React, { Component } from 'react';
 
-class Filter extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      filters: []
-    };
-  }
-
-  componentWillMount() {
-    this.fetchFilters();
-  }
-
-  fetchFilters = () => {
-    fetch('/api/fruit_types')
-      .then(response => response.json())
-      .then(filters => this.setState({ filters }));
-  }
-
-  render() {
+const Filter = ({filters, handleChange}) => {
     return (
       <select onChange={this.props.handleChange} defaultValue='all'>
         <option value='all'>All</option>
@@ -29,6 +10,8 @@ class Filter extends Component {
       </select>
     );
   }
+Filter.defaultProps = {
+  filters: [],
+  handleChange: null
 }
-
 export default Filter;
